fba60789268bc79999aad785daa0425493abedaf
Matuteze <matu_diaz_95@hotmail.com>
2021-04-17 00:12:23 -0300

    Intento de salto

--- 1/Entities/Player/Player.gd
+++ 1/Entities/Player/Player.gd
@@ -1,6 +1,8 @@
 extends Sprite
 
 export (float) var speed:float = 20
+export (float) var gravity:float = 50
+export (float) var jumpForce:float = -100
 
 func _process(delta):
 #	var right:bool = Input.is_action_pressed("Derecha")
@@ -8,10 +10,15 @@ func _process(delta):
 	var up:bool = Input.is_action_pressed("Salto")
 	
 	if up:
-		while(position.y>-10):
-			position.y = -speed * delta
-		while(position.y<0):
-			position.y = speed * delta
+		var buttomJump:float = position.y 
+		var jump:float = position.y 
+		var topJump:float = jump + jumpForce
+		while jump > topJump:
+			position.y -= delta * gravity
+			jump = position.y
+		while jump < buttomJump :
+			position.y += delta * gravity
+			jump = position.y
 			
 	var direction:int = int(Input.is_action_pressed("Derecha")) - int(Input.is_action_pressed("Izquierda"))
 	position.x += direction * speed * delta
